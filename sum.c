692ae403ca4c2a089ea4b52b3c7061e16fcc47bc
Ankitweblogic <ankitweblogic@gmail.com>
2024-05-28 13:01:31 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int a, b, c;
+    printf("Enter 1st number:");
+    scanf("%d",&a);
+    printf("Enter 2nd Number:");
+    scanf("%d",&b);
+    c=a+b;
+    printf("Sum=%d",c);
+}
